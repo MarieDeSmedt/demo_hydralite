d2bade35e363396dd6ea60086fc33896baa3de4b
MarieDeSmedt <desmedtmarie@outlook.com>
2022-01-05 15:17:30 +0100

    inpu_date

--- page/sidebar.py
+++ page/sidebar.py
@@ -1,12 +1,29 @@
 import streamlit as st
 import pandas as pd
+import datetime
 
 def display_sidebar():
     mon_perimetre={}
 
-    #choose sector
+    #sector
     choosed_sector = st.sidebar.multiselect("SECTEURS:",st.session_state['authorised_sectors'])
     mon_perimetre["sector"] = choosed_sector
+
+    #periode
+    start = st.sidebar.date_input('start date :')
+    end = st.sidebar.date_input('end date :')
+    mon_perimetre["start"]=start
+    mon_perimetre["end"]=end
+
+
+    
+    
+    #siteCluster
+    #typeTicket	
+    #brandType	
+    #segmentation>SegmentValue>SegmentLabel	
+    #custAgeRange	
+    #customerActivationEasinessLabel	
 
     if st.sidebar.button("définir périmètre"):
         st.session_state['mon_perimetre']= mon_perimetre
