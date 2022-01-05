89ba9d862403f3dd5dfd5d6cb9271af33c48943b
MarieDeSmedt <desmedtmarie@outlook.com>
2022-01-05 15:35:03 +0100

    multiselect perimetre sidebar

--- page/sidebar.py
+++ page/sidebar.py
@@ -6,24 +6,37 @@ def display_sidebar():
     mon_perimetre={}
 
     #sector
-    choosed_sector = st.sidebar.multiselect("SECTEURS:",st.session_state['authorised_sectors'])
-    mon_perimetre["sector"] = choosed_sector
+    mon_perimetre["sector"] = st.sidebar.multiselect("SECTEUR:",st.session_state['authorised_sectors'])
 
     #periode
-    start = st.sidebar.date_input('start date :')
-    end = st.sidebar.date_input('end date :')
-    mon_perimetre["start"]=start
-    mon_perimetre["end"]=end
+    mon_perimetre["start"] = st.sidebar.date_input('DEBUT:')
+    mon_perimetre["end"] = st.sidebar.date_input('FIN:')
 
 
-    
-    
     #siteCluster
+    siteCluster=['CLU01','CLU02','CLU03','CLU04','CLU05','CLU06']
+    mon_perimetre['siteCluster'] = st.sidebar.multiselect("CLUSTER",siteCluster)
+
     #typeTicket	
-    #brandType	
+    typeTicket=['SUPER','HYPER','DRIVE']
+    mon_perimetre['typeTicket'] = st.sidebar.multiselect("FORMAT:",typeTicket)
+
+    #brandType
+    brandType=['MP','MN','NO','PP','Autre']
+    mon_perimetre['brandType'] = st.sidebar.multiselect("MARQUE:",brandType)
+
+    #custAgeRange
+    custAgeRange=['18-24','25-29','30-34','35-39','40-44','45-49','50-54','55-59','60-64','65-69','70+']
+    mon_perimetre['custAgeRange'] = st.sidebar.multiselect("AGE:",custAgeRange)
+
+    #customerActivationEasinessLabel
+    customerActivationEasinessLabel=['Non géolocalisés','Hors zone','Très facile','Très difficile','Difficile',
+                                        'Moyen','Facile']
+    mon_perimetre['customerActivationEasinessLabel'] = st.sidebar.multiselect("ACTIVATION:",customerActivationEasinessLabel)
+
     #segmentation>SegmentValue>SegmentLabel	
-    #custAgeRange	
-    #customerActivationEasinessLabel	
+    #TODO	
+    	
 
     if st.sidebar.button("définir périmètre"):
         st.session_state['mon_perimetre']= mon_perimetre
