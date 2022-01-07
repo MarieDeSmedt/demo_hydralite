b8dab14add96e0dfb817f2b8551891f22096f819
MarieDeSmedt <desmedtmarie@outlook.com>
2022-01-07 15:15:02 +0100

    change home

--- page/home.py
+++ page/home.py
@@ -2,9 +2,7 @@ import streamlit as st
 import pandas as pd
 
 def display_home():
-    #affiche le message d'acceuil en fonction des secteurs auxquels la personne a accés
-    df_sectors = pd.read_csv("data/sector_label.csv")
-    df=df_sectors[df_sectors['sector'].isin(st.session_state['authorised_sectors'])]
-    st.write("vous avez accés aux secteurs:")
-    for index, row in df.iterrows():
-        st.write(row[0],":", row[1])
+    st.title("pour l'exemple les filtres a prendre sont:")
+    st.write("le secteur textile")
+    st.write("en hyper")
+    st.write("1er semestre 2021")
